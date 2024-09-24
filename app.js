e51740ad0230ce78bedd94caabbbd875e431d691
Nifemi-Joe <nifemijoseph8@gmail.com>
2024-09-24 12:40:51 +0100

    rate module added

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 
 const corsOptions = {
