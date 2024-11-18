e096567b5d5a1f58bdf2b660cb4697296dbb78f8
Nifemi-Joe <nifemijoseph8@gmail.com>
2024-11-18 10:22:08 +0000

    fix

--- routes/auditLogRoutes.js
+++ routes/auditLogRoutes.js
@@ -6,6 +6,6 @@ const { getAuditLogs } = require('../controllers/auditLogController');
 const { protect, authorize, authorizePermissions } = require('../middlewares/authMiddleware');
 
 // Get All Audit Logs
-router.get('/', protect, authorize('admin', 'superadmin'), authorizePermissions('view-logs'),  getAuditLogs);
+router.get('/', protect, authorize('admin', 'superadmin'),  getAuditLogs);
 
 module.exports = router;
