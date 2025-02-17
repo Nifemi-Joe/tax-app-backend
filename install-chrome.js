7b82816d0bbea86613e48bd11524a0ea06934390
Nifemi-Joe <nifemijoseph8@gmail.com>
2025-02-17 06:14:35 +0000

    Update install-chrome.js for Puppeteer v19+

--- install-chrome.js
+++ install-chrome.js
@@ -1,11 +1,8 @@
-const fs = require('fs');
 const puppeteer = require('puppeteer');
 
 async function installChromium() {
-	const browserFetcher = puppeteer.createBrowserFetcher();
-	const revisionInfo = await browserFetcher.download('900000'); // Download a specific version of Chromium
-
-	fs.writeFileSync('./chromium', revisionInfo.executablePath); // Save the path to the browser
+	const revision = await puppeteer.downloadBrowser(); // New method to download Chromium
+	console.log('Chromium downloaded:', revision);
 }
 
 installChromium();
