b6c36f8f8da951abcc3bb6efb4094a1166f368fa
Nifemi-Joe <nifemijoseph8@gmail.com>
2025-11-07 00:52:25 +0000

    webmail fix

--- utils/emailService.js
+++ utils/emailService.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // Try multiple port/security configurations
 const SMTP_CONFIGS = [
 	{
-		host: "webmail.globalsjxltd.com",
+		host: "smtp.mailhostbox.com",
 		port: 587,
 		secure: false, // Use TLS
 		auth: {
